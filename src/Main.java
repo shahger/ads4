7dc1a255c170a946cdad53f7fbdca02468fa5b4c
shahger <shahgoer@gmail.com>
2026-05-14 00:34:54 +0500

    feat(traversal): added BFS and DFS

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
-void main() {
-  $END$
+public class Main {
+    public static void main(String[] args) {
+        Graph small = Experiment.createGraph(10);
+        Experiment.runTraversals(small, "Small (10 vertices)");
+
+        Graph medium = Experiment.createGraph(30);
+        Experiment.runTraversals(medium, "Medium (30 vertices)");
+
+        Graph large = Experiment.createGraph(100);
+        Experiment.runTraversals(large, "Large (100 vertices)");
+    }
 }
